74cc7243d21872cfdbf41836165b41db9d08950a
Max Monis <mmonis77@gmail.com>
2020-09-15 07:58:39 -0400

    remove trailing slash from backendURL

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@ module.exports = {
   env: {
     backendURL:
       process.env.NODE_ENV === 'production'
-        ? 'https://stormy-bastion-27700.herokuapp.com/'
+        ? 'https://stormy-bastion-27700.herokuapp.com'
         : 'http://localhost:4000',
     frontendURL:
       process.env.NODE_ENV === 'production'
